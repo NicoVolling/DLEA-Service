793a98d53de862a3a09a5b6d349094319a0d7301
Nico Volling <nico.volling.2000@gmail.com>
2022-01-17 17:01:19 +0100

    Blinker

--- Client/Menu/Menu.Submenu_Tools.cs
+++ Client/Menu/Menu.Submenu_Tools.cs
@@ -33,6 +33,31 @@ namespace Client.Menu
                 Game.PlayerPed.CurrentVehicle?.Repair();
             }));
 
+            //AddMenuItem(Submenu_Tools_Vehicle, "Blinker Links", "Blinker Links", new Action<UIMenuItem>((item) =>
+            //{
+            //    if(Game.PlayerPed.IsInVehicle() && Game.PlayerPed.CurrentVehicle is Vehicle CurrentVehicle) 
+            //    {
+            //        CurrentVehicle.IsLeftIndicatorLightOn = !CurrentVehicle.IsLeftIndicatorLightOn;
+            //    }
+            //}));
+
+            //AddMenuItem(Submenu_Tools_Vehicle, "Blinker Rechts", "Blinker Rechts", new Action<UIMenuItem>((item) =>
+            //{
+            //    if (Game.PlayerPed.IsInVehicle() && Game.PlayerPed.CurrentVehicle is Vehicle CurrentVehicle)
+            //    {
+            //        CurrentVehicle.IsRightIndicatorLightOn = !CurrentVehicle.IsRightIndicatorLightOn;
+            //    }
+            //}));
+
+            //AddMenuItem(Submenu_Tools_Vehicle, "Warnblinker", "Warnblinker", new Action<UIMenuItem>((item) =>
+            //{
+            //    if (Game.PlayerPed.IsInVehicle() && Game.PlayerPed.CurrentVehicle is Vehicle CurrentVehicle)
+            //    {
+            //        CurrentVehicle.IsLeftIndicatorLightOn = !(CurrentVehicle.IsLeftIndicatorLightOn && CurrentVehicle.IsRightIndicatorLightOn);
+            //        CurrentVehicle.IsRightIndicatorLightOn = !(CurrentVehicle.IsLeftIndicatorLightOn && CurrentVehicle.IsRightIndicatorLightOn);
+            //    }
+            //}));
+
             UIMenu Submenu_Tools_Player = MenuPool.AddSubMenu(Submenu_Tools, "Spieler", "Spieler");
 
             AddMenuItem(Submenu_Tools_Player, "Schnellreinigung", "Spieler reinigen", (item) => 
